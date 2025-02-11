8f049250aaa4574dd1c10a715f4b199f2824f826
Alejandrfrancocardenas <alejandro.francoc1@uqvirtual.edu.co>
2025-02-11 17:51:15 -0500

    Se agrego ejemplos estructurales de java

---
+++ Info/Projectoclas1/src/Basic.java
@@ -0,0 +1,72 @@
+public class Basic {
+//Tipo de acceso - TipoRetorno - nombre - (parametros <opcional>)
+
+    private void test(int x, String text, boolean logico) {
+        //codigo del metodo
+
+
+    }
+
+    //ejemplos retornos
+    private String test3() {
+        return "";
+    }
+
+    private int test4() {
+        return 0;
+
+    }
+
+    //ejemplos datos
+    private void test2() {
+        int num = 2;
+        String text = "si";
+        double cifra = 0.0;
+        boolean logico = true;
+
+    }
+    //ejemplos condicionales
+
+    private void test6() {
+
+
+        if (condicion) {
+        } else if (condicion2) {
+        } else {
+
+        }
+
+        //
+        switch (condicion) {
+
+
+            case option1:
+                //codigo
+                break;
+
+            case option2:
+                //codigo
+                break;
+            default:
+                //codigo
+                break;
+        }
+    }
+        //ejemplo ciclos
+        private void test7() {
+        //ciclo for (punto de partida),(punto de llagada),(aumento de la escala)
+    
+        for(int i = 1; i <= 10; i++) {
+            //codigo
+
+        }
+        while (condicion){
+        //codigo
+        }
+        //ciclo do while
+
+        do {
+
+        }while(condicion);
+}
+}
